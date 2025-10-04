abf95c02e8762b945360753b2f25dc19e5c0c551
Kim <kimberlybarreto2015@gmail.com>
2025-10-04 11:56:25 -0400

    Tratando de hacer el login

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import 'react-native-gesture-handler';
+import Login from './screens/Login';
+import Home from './sreens/Home';
 
 export default function App() {
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+
+
+  return (
+  <NavigationContainer>
+    <MyStack/>
+  </NavigationContainer>
   );
 }
 
---
+++ sreens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      <Text> Login </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
